4e1ea5db9491288a3b02e2e1a3e26961a78294bc
kim <kimkimani016@gmail.com>
2020-02-10 12:55:17 +0300

    Initial commit

---
+++ app/src/main/java/com/e/basetest/ProductsAdapter.java
@@ -0,0 +1,70 @@
+package com.e.basetest;
+
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Belal on 10/18/2017.
+ */
+
+public class ProductsAdapter extends RecyclerView.Adapter<ProductsAdapter.ProductViewHolder> {
+
+
+    private Context mCtx;
+    private List<Product> productList;
+
+    public ProductsAdapter(Context mCtx, List<Product> productList) {
+        this.mCtx = mCtx;
+        this.productList = productList;
+    }
+
+    @Override
+    public ProductViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(mCtx);
+        View view = inflater.inflate(R.layout.listtips, null);
+        return new ProductViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ProductViewHolder holder, int position) {
+        Product product = productList.get(position);
+
+        //loading the imag
+
+        holder.textViewTitle.setText(product.getTitle());
+        holder.textViewShortDesc.setText(product.getShortdesc());
+        holder.textViewRating.setText(String.valueOf(product.getRating()));
+        holder.textViewShortDesc.setText((int) product.getPrice());
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return productList.size();
+    }
+
+    class ProductViewHolder extends RecyclerView.ViewHolder {
+
+        TextView textViewTitle, textViewShortDesc, textViewRating, textViewPrice;
+        ImageView imageView;
+
+        public ProductViewHolder(View itemView) {
+            super(itemView);
+
+            textViewTitle = itemView.findViewById(R.id.textViewTitle);
+            textViewShortDesc = itemView.findViewById(R.id.textViewShortDesc);
+            textViewRating = itemView.findViewById(R.id.textViewRating);
+            textViewPrice = itemView.findViewById(R.id.textViewPrice);
+
+        }
+    }
+}
